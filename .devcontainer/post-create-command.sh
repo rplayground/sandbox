8c0a238f4dbf22b86b97b3068fdb4c0eef4f7334
ruffsl <roxfoxpox@gmail.com>
2023-05-24 19:07:43 +0200

    Fix absolute path

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -13,6 +13,6 @@ cp /etc/skel/.bashrc ~/
 # Check if srv folder exists
 if [ -d "$ROOT_SRV" ]; then
     # Setup Nav2 web app
-    for dir in .devcontainer/caddy/srv/*; \
+    for dir in $PWD/.devcontainer/caddy/srv/*; \
         do if [ -d "$dir" ]; then ln -s "$dir" $ROOT_SRV; fi done
 fi
