bfc9d0e0e6bb3e603fbf89b4e896e4b211f40845
ruffsl <roxfoxpox@gmail.com>
2023-05-24 18:01:14 +0200

    Simplify Dev Container config

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -13,6 +13,6 @@ cp /etc/skel/.bashrc ~/
 # Check if srv folder exists
 if [ -d "$ROOT_SRV" ]; then
     # Setup Nav2 web app
-    for dir in $OVERLAY_WS/src/navigation2/.devcontainer/caddy/srv/*; \
+    for dir in .devcontainer/caddy/srv/*; \
         do if [ -d "$dir" ]; then ln -s "$dir" $ROOT_SRV; fi done
 fi
